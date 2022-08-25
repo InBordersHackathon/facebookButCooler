e1fe034cd4711df43a2335d38782fe8ec86977e2
Steve Ilardi <86074712+steveilardi@users.noreply.github.com>
2022-08-25 16:25:32 -0400

    fix UI attempt

--- example-app/src/App.js
+++ example-app/src/App.js
@@ -2,13 +2,32 @@ import './App.css';
 
 function App() {
   async function getData() {
-    const resp = await fetch('http://localhost:3001/', {
-      method: 'get'
-    });
-    return await resp.json();
+    // const resp = await fetch('http://localhost:3001/', {
+    //   method: 'get'
+    //   // mode: 'no-cors'
+    // });
+    // return await resp.json();
+    try {
+      const response = await fetch('http://localhost:3001/', {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
   };
 
  const data = getData();
+ console.log("data", data);
   
   return (
     <div className="App">
--- example-backend/index.js
+++ example-backend/index.js
@@ -2,6 +2,11 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const cors = require('cors');
+
+app.use(cors({
+    origin: '*'
+}));
 
 app.get('/', (req, res) => {
   res.json({ data: 'Hello World!' })
