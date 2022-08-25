fb9680121350f5d9eca5230b90ff841d6d1eba25
stivenBermeoXbs <stiven.bermeo@xbs.ai>
2022-08-25 15:59:21 -0500

    thifgnasda

--- example-app/src/App.js
+++ example-app/src/App.js
@@ -1,39 +1,30 @@
 import './App.css';
 
 function App() {
-  async function getData() {
-    // const resp = await fetch('http://localhost:3001/', {
-    //   method: 'get'
-    //   // mode: 'no-cors'
-    // });
-    // return await resp.json();
-    try {
-      const response = await fetch('http://localhost:3001/', {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
-      }
-  
-      const result = await response.json();
-      return result;
-    } catch (err) {
-      console.log(err);
-    }
-  };
+ 
 
- const data = getData();
- console.log("data", data);
+  const getData = async function (url = '') {
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow', // manual, *follow, error
+    });
+    return response?.json();
+  }
+
+  let text = "";
+
+ getData('http://localhost:3001').then(data => {
+  console.log({data});
+  text = data;
+ });
   
   return (
     <div className="App">
-      <header className="App-header">
-        {{ data }}
-      </header>
+      <div>
+      {text}
+      </div>
     </div>
   );
 }
